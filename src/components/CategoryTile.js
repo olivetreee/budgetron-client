34b05fddb5292468bf5be7fa99ebe61a071a74d7
Eric Oliveira <eoliveira@indeed.com>
2022-12-11 06:02:37 -0800

    master: Add react-circular-progressbar

--- src/components/CategoryTile.js
+++ src/components/CategoryTile.js
@@ -1,15 +1,29 @@
 import { CATEGORY_ICON } from "../constants";
 import './CategoryTile.css';
+import 'react-circular-progressbar/dist/styles.css';
+
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 
 export const CategoryTile = ({
   category,
   limit,
   amountSpent
 }) => {
+  const percentageConsumed = Math.ceil((amountSpent/limit) * 100);
+  let status = "good";
+  if (percentageConsumed > 50) status = "warning";
+  if (percentageConsumed > 75) status = "danger";
+
   return (
     <>
-      <div className={`CategoryTile ${category.split(" ").join("")}`}>
-        <i className={`fa fa-light ${CATEGORY_ICON[category]}`} />
+      <div className={`category-tile ${category.split(" ").join("")} ${status}`}>
+      <CircularProgressbarWithChildren value={percentageConsumed}>
+        <div className="circular-progress-bar-content">
+          <p className="amount-spent">${Math.ceil(amountSpent)}</p>
+          <p className="limit">${limit}</p>
+        </div>
+      </CircularProgressbarWithChildren>
+    {/* <i className={`fa fa-light ${CATEGORY_ICON[category]}`} />
         <div className="right-column">
           <div className="text">
             <h2>{category}</h2>
@@ -22,7 +36,7 @@ export const CategoryTile = ({
             </div>
           </div>
           <div className="progress-bar" />
-        </div>
+        </div> */}
       </div>
     </>
   )

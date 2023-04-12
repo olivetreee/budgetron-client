0b5feedb1047871cc6fc2acfcea4a84f943706cf
Eric Oliveira <eoliveira@indeed.com>
2023-04-12 10:19:34 -0700

    main: Scroll CategoryDropdown list instead of whole page

--- src/components/CategoriesDropdown.jsx
+++ src/components/CategoriesDropdown.jsx
@@ -5,16 +5,26 @@ import { useCategories } from "../providers/CategoriesProvider";
 export const CategoriesDropdown = ({ currentValue, onChange }) => {
   const [{ categoriesByType: { expense, intake } }] = useCategories();
   return (
-    <DropdownButton variant="light" onSelect={onChange} title={currentValue}>
-      <Dropdown.Header>Expense</Dropdown.Header>
-      {expense.map(category => (
-        <Dropdown.Item key={category} eventKey={category}>{category}</Dropdown.Item>
-      ))}
-      <Dropdown.Divider />
-      <Dropdown.Header>Intake</Dropdown.Header>
-      {intake.map(category => (
-        <Dropdown.Item key={category} eventKey={category}>{category}</Dropdown.Item>
-      ))}
-    </DropdownButton>
+    <>
+      <style type="text/css">
+        {`
+          .dropdown-menu {
+            overflow: scroll;
+            height: 30rem;
+          }
+        `}
+      </style>
+      <DropdownButton variant="light" onSelect={onChange} title={currentValue}>
+        <Dropdown.Header>Expense</Dropdown.Header>
+        {expense.map(category => (
+          <Dropdown.Item key={category} eventKey={category}>{category}</Dropdown.Item>
+        ))}
+        <Dropdown.Divider />
+        <Dropdown.Header>Intake</Dropdown.Header>
+        {intake.map(category => (
+          <Dropdown.Item key={category} eventKey={category}>{category}</Dropdown.Item>
+        ))}
+      </DropdownButton>
+    </>
   );
 };
